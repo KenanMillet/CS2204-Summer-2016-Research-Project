46651a50444a5181523ff0f061b7e4741a072f22
anabolsoni <anabolsoni@nyu.edu>
2016-06-15 13:25:46 -0400

    Simple Project changes with C++ code and bit file

---
+++ ppm-revamp/Xilinx-Files/Simple/complex/complex/Debug/src/platform.d
@@ -0,0 +1,22 @@
+src/platform.d: ../src/platform.c \
+ ../../complex_bsp/communication/include/xparameters.h \
+ ../../complex_bsp/communication/include/xil_cache.h \
+ ../../complex_bsp/communication/include/mb_interface.h \
+ ../../complex_bsp/communication/include/xbasic_types.h \
+ ../../complex_bsp/communication/include/xil_types.h \
+ ../../complex_bsp/communication/include/xparameters.h \
+ ../src/platform_config.h
+
+../../complex_bsp/communication/include/xparameters.h:
+
+../../complex_bsp/communication/include/xil_cache.h:
+
+../../complex_bsp/communication/include/mb_interface.h:
+
+../../complex_bsp/communication/include/xbasic_types.h:
+
+../../complex_bsp/communication/include/xil_types.h:
+
+../../complex_bsp/communication/include/xparameters.h:
+
+../src/platform_config.h:
---
+++ ppm-revamp/Xilinx-Files/Simple/complex/complex/Debug/src/subdir.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/simpleproject.cpp 
+
+C_SRCS += \
+../src/platform.c 
+
+LD_SRCS += \
+../src/lscript.ld 
+
+OBJS += \
+./src/platform.o \
+./src/simpleproject.o 
+
+C_DEPS += \
+./src/platform.d 
+
+CPP_DEPS += \
+./src/simpleproject.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MicroBlaze g++ compiler'
+	mb-g++ -Wall -O3 -g3 -c -fmessage-length=0 -I../../complex_bsp/communication/include -mno-xl-reorder -mlittle-endian -mcpu=v8.40.a -mxl-soft-mul -Wl,--no-relax -ffunction-sections -fdata-sections -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: MicroBlaze g++ compiler'
+	mb-g++ -Wall -O3 -g3 -c -fmessage-length=0 -I../../complex_bsp/communication/include -mno-xl-reorder -mlittle-endian -mcpu=v8.40.a -mxl-soft-mul -Wl,--no-relax -ffunction-sections -fdata-sections -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
