46bd7a736877ab117c622f9535a4daf7aceaf438
Kenan Millet <KVMillet@Gmail.com>
2016-08-09 13:49:09 -0400

    Updated the code for communication to work with a single player
    it might be expanded if time allows, but this is just for testing
    purposes

--- pong/MB/communication/src/main.cc
+++ pong/MB/communication/src/main.cc
@@ -2,56 +2,50 @@
 #include "platform.h"
 #include <xparameters.h>
 #include <xiomodule.h>
-#include "Command.h"
+#include <math.h>
 
 void init_gpio();
 void init_uart_interrupt();
-void init_gpi_interrupt();
-void init_uart_polling();
 
 void rx_interrupt(void*);
-void gpi_interrupt(void*);
-
-u32 pollUSB();
 void sendUSB(u32);
 
+
+
+u16 argToInt(char[4]);
+
+char args[6][4];
+bool writegpo;
+
 XIOModule gpio;
-XIOModule gpi_int;
 XIOModule uart;
 
-u32 gpoData;
-
-
+u32 gpoData[4];
 
 int main()
 {
 	init_platform();
 	init_gpio();
 	init_uart_interrupt();
+
+
+	while(true)
+	{
+		if(writegpo)
+		{
+			XIOModule_Uart_DisableInterrupt(&uart);
+			for(u8 i = 0; i < 4; ++i) XIOModule_DiscreteWrite(&gpio, i+1, gpoData[i]);
+			sendUSB(XIOModule_DiscreteRead(&gpio, 1));
+			XIOModule_Uart_EnableInterrupt(&uart);
+		}
+	}
+	return 0;
 }
 
 void init_gpio()
 {
 	XIOModule_Initialize(&gpio, XPAR_IOMODULE_0_DEVICE_ID);
 	XIOModule_Start(&gpio);
-	gpoData = 0;
-}
-
-void init_gpi_interrupt()
-//doesn't work with this serial comm code. Problem starts somewhere around Connect function.
-{
-	XIOModule_Initialize(&gpi_int, XPAR_IOMODULE_0_DEVICE_ID); // Initialize the GPO module
-
-	microblaze_register_handler(XIOModule_DeviceInterruptHandler,
-								  XPAR_IOMODULE_0_DEVICE_ID); // register the interrupt handler
-
-	XIOModule_Start(&gpi_int); // start the GPO module
-
-	XIOModule_Connect(&gpi_int, XIN_IOMODULE_GPI_1_INTERRUPT_INTR, (XInterruptHandler)gpi_interrupt,
-							NULL); // register timerTick() as our interrupt handler
-	XIOModule_Enable(&gpi_int, XIN_IOMODULE_GPI_1_INTERRUPT_INTR); // enable the interrupt
-
-	microblaze_enable_interrupts(); // enable global interrupts
 }
 
 void init_uart_interrupt()
@@ -69,26 +63,37 @@ void init_uart_interrupt()
 	XIOModule_Start(&uart); // start the GPO module
 }
 
-void init_uart_polling()
-{
-}
 
 void rx_interrupt(void*)
 {
+	static u8 i(0);
+
+	if(i == 0) for(u8 j = 0; j < 4; ++j) gpoData[j] = 0;
+
+	u8 byte = XIOModule_RecvByte(STDIN_BASEADDRESS);
+	if(byte == ' ') return;
+
+	args[i/4][i%4] = byte;
+	if((((i+1)/4)*4) == (i+1)) gpoData[i/8] |= argToInt(args[i/4]) << 10*((i/4)%2);
+	i++;
+	i %= 24;
+	writegpo = (i==0);
+	return;
 }
 
-u32 pollUSB()
+void sendUSB(u32 data)
 {
-
+	xil_printf("%c\n", char(data));
 }
 
-void sendUSB(u32)
-{
 
-}
 
-void gpi_interrupt(void*)
+u16 argToInt(char str[4])
 {
-//probably wouldn't work right even if we could initalize it properly
-	sendUSB(XIOModule_DiscreteRead(&gpio, 1));
+	u16 result = 0;
+	for(u8 i = 0; i < 4; ++i)
+	{
+		result += (str[i] - '0') * pow(10, i);
+	}
+	return result;
 }
